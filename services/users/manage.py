1f852f1e957dfeb332105cbee930348d872c6bac
brandux <you@example.com>
2019-04-22 23:13:55 -0500

    a

---
+++ services/users/manage.py
@@ -0,0 +1,38 @@
+# services/users/manage.py
+
+import unittest
+
+
+from flask.cli import FlaskGroup
+
+from project import create_app, db # new
+from project.api.models import User # new
+
+app = create_app() # new
+cli = FlaskGroup(create_app=create_app) # new
+
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command()
+def test():
+    """Ejecutar los tests sin covertura de codigo"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+# nueva era
+@cli.command('seed_db')
+def seed_db():
+    """Sembrado en la base de datos"""
+    db.session.add(User(username='Brandux JUarez', email= 'branduxjuarez@upeu.edu.pe'))
+    db.session.add(User(username='Didier', email= 'didi@upeu.edu.pe'))
+    db.session.commit()
+
+if __name__ == '__main__':
+    cli()
